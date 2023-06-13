e2e66344eaaec19a0bf9df5c910a7ac3ecdc5a57
Zach Burnaby <58493767+TheRedLancer@users.noreply.github.com>
2023-06-13 16:23:33 -0700

    Start help luke

--- GameplayScripts/extractor.py
+++ GameplayScripts/extractor.py
@@ -64,8 +64,25 @@ def minions_in_area(x1, x2, y1, y2, positions):
     return len([tuple for tuple in positions if tuple[0] <= x2 and tuple[0] >= x1 and tuple[1] <= y2 and tuple[1] >= y1])
 
 
+team = {
+    100: "blue",
+    200: "red"
+}
+
+minion_names = {
+    "sru_chaosminionranged": "r_ranged",
+    "sru_chaosminionsiege": "r_siege",
+    "sru_chaosminionmelee": "r_melee",
+    "sru_chaosminionsuper": "r_super",
+    "sru_orderminionranged": "b_ranged",
+    "sru_orderminionsiege": "b_siege",
+    "sru_orderminionmelee": "b_melee",
+    "sru_orderminionsuper": "b_super",
+}
+
+
 def dump(obj):
-    print("dump")
+    # print("dump")
     global snapshot_index
     global gametime
     global page_index
@@ -75,6 +92,7 @@ def dump(obj):
     gametime = obj.time
 
     champlist = obj.champs
+
     others = []
     for item in obj.others:
         if item.name in c:
@@ -82,29 +100,37 @@ def dump(obj):
         else:
             others.append({
                 "name": item.name,
-                "x": item.pos.x,
-                "z": item.pos.z,
-                "health": item.health,
-                "max_health": item.max_health
+                "x": round(item.pos.x, 4),
+                "z": round(item.pos.z, 4),
+                "hp": round(item.health, 4),
+                "max_hp": item.max_health
             })
-    jg = []
     jungle = []
-    [jungle.append({
-        "name": item.name,
-        "x": item.pos.x,
-        "z": item.pos.z,
-        "health": item.health,
-        "max_health": item.max_health
-    }) for item in obj.jungle]
-    [jg.append(x.name) for x in obj.jungle]
-    minions = []
-    [minions.append({
-        "name": item.name,
-        "x": item.pos.x,
-        "z": item.pos.z,
-        "health": item.health,
-        "max_health": item.max_health
-    }) for item in obj.minions]
+    dragon = False
+    herald = False
+    baron = False
+    for item in obj.jungle:
+        jungle.append({
+            "name": item.name,
+            "x": round(item.pos.x, 4),
+            "z": round(item.pos.z, 4),
+            "hp": round(item.health, 4),
+            "max_hp": item.max_health
+        })
+        if (item.name == 'dragon'):
+            dragon = True
+        if (item.name == 'herald'):
+            herald = True
+        if (item.name == 'baron'):
+            baron = True
+
+    minions = [{
+        "name": minion_names[item.name],
+        "x": round(item.pos.x, 4),
+        "z": round(item.pos.z, 4),
+        "hp": round(item.health, 4),
+        "max_hp": item.max_health
+    } for item in obj.minions]
 
     # redminionpos = [(minion.pos.x, minion.pos.z)
     #                 for minion in obj.minions if "chaos" in minion.name]
@@ -164,121 +190,116 @@ def dump(obj):
     # if bot_wave_pushed == 'N/A':
     #     bot_wave_pushed = minionadv(red_bot_inner, blue_bot_inner)
 
-    d = {"champs": [],
-         "time": obj.time,
-         "index": snapshot_index,
-         "jungle": jungle,
-         "minons": minions,
-         "others": others,
-         "dragon_up": any('dragon' in x for x in jg),
-         "herald_up": any('herald' in x for x in jg),
-         "baron_up": any('baron' in x for x in jg),
-         # "red_minions": redminionpos,
-         # "blue_minions": blueminionpos,
-         #  "red_minions_top_inner": red_top_inner,
-         #  "blue_minions_top_inner": blue_top_inner,
-         #  "red_minions_mid_inner": red_mid_inner,
-         #  "blue_minions_mid_inner": blue_mid_inner,
-         #  "red_minions_bot_inner": red_bot_inner,
-         #  "blue_minions_bot_inner": blue_bot_inner,
-         #  "blue_side_top_l2_blue_minions": minions_in_area(900, 2500, 6699, 10350, blueminionpos),
-         #  "blue_side_top_l2_red_minions": minions_in_area(900, 2500, 6699, 10350, redminionpos),
-         #  "blue_side_top_l3_blue_minions": minions_in_area(900, 2500, 4252, 6699, blueminionpos),
-         #  "blue_side_top_l3_red_minions": minions_in_area(900, 2500, 4252, 6699, redminionpos),
-         #  "blue_side_mid_l2_blue_minions": minions_in_area(5048, 5800, 4812, 6200, blueminionpos),
-         #  "blue_side_mid_l2_red_minions": minions_in_area(5048, 5800, 4812, 6200, redminionpos),
-         #  "blue_side_mid_l3_blue_minions": minions_in_area(3620, 5048, 3696, 4812, blueminionpos),
-         #  "blue_side_mid_l3_red_minions": minions_in_area(3651, 5048, 3696, 4812, redminionpos),
-         #  "blue_side_bot_l2_blue_minions": minions_in_area(6919, 10400, 900, 2500, blueminionpos),
-         #  "blue_side_bot_l2_red_minions": minions_in_area(6919, 10400, 900, 2500, redminionpos),
-         #  "blue_side_bot_l3_blue_minions": minions_in_area(4281, 6919, 900, 2500, blueminionpos),
-         #  "blue_side_bot_l3_red_minions": minions_in_area(4281, 6919, 900, 2500, redminionpos),
-         #  "blue_side_base_blue_minions": minions_in_area(0, 3651, 0, 3696, blueminionpos),
-         #  "blue_side_base_red_minions": minions_in_area(0, 3651, 0, 3696, redminionpos),
-         #  "red_side_top_l2_blue_minions": minions_in_area(4550, 7952, 12500, 14000, blueminionpos),
-         #  "red_side_top_l2_red_minions": minions_in_area(4550, 7952, 12500, 14000, redminionpos),
-         #  "red_side_top_l3_blue_minions": minions_in_area(7952, 10481, 12500, 14000, blueminionpos),
-         #  "red_side_top_l3_red_minions": minions_in_area(7952, 10481, 12500, 14000, redminionpos),
-         #  "red_side_mid_l2_blue_minions": minions_in_area(8955, 9767, 8510, 10113, blueminionpos),
-         #  "red_side_mid_l2_red_minions": minions_in_area(8955, 9767, 8510, 10113, redminionpos),
-         #  "red_side_mid_l3_blue_minions": minions_in_area(9767, 11134, 10113, 11207, blueminionpos),
-         #  "red_side_mid_l3_red_minions": minions_in_area(9767, 11134, 10113, 11207, redminionpos),
-         #  "red_side_bot_l2_blue_minions": minions_in_area(12000, 14500, 4505, 8226, blueminionpos),
-         #  "red_side_bot_l2_red_minions": minions_in_area(12000, 14500, 4505, 8226, redminionpos),
-         #  "red_side_bot_l3_blue_minions": minions_in_area(12000, 14500, 8226, 11207, blueminionpos),
-         #  "red_side_bot_l3_red_minions": minions_in_area(12000, 14500, 8226, 11207, redminionpos),
-         #  "red_side_base_blue_minions": minions_in_area(11134, 11207, 18000, 18000, blueminionpos),
-         #  "red_side_base_red_minions": minions_in_area(11134, 11207, 18000, 18000, redminionpos),
-         #  "minion_advantage_top_inner": minionadv(red_top_inner, blue_top_inner),
-         #  "minion_advantage_mid_inner": minionadv(red_mid_inner, blue_mid_inner),
-         #  "minion_advantage_bot_inner": minionadv(red_bot_inner, blue_bot_inner),
-         #  "top_lane_pushed": top_wave_pushed,
-         #  "mid_lane_pushed": mid_wave_pushed,
-         #  "bot_lane_pushed": bot_wave_pushed
-         }
+    d = {
+        "champs": [],
+        "time": round(obj.time, 5),
+        "index": snapshot_index,
+        "jungle": jungle,
+        "minions": minions,
+        "others": others,
+        "dragon": dragon,
+        "herald": herald,
+        "baron": baron,
+        # "red_minions": redminionpos,
+        # "blue_minions": blueminionpos,
+        #  "red_minions_top_inner": red_top_inner,
+        #  "blue_minions_top_inner": blue_top_inner,
+        #  "red_minions_mid_inner": red_mid_inner,
+        #  "blue_minions_mid_inner": blue_mid_inner,
+        #  "red_minions_bot_inner": red_bot_inner,
+        #  "blue_minions_bot_inner": blue_bot_inner,
+        #  "blue_side_top_l2_blue_minions": minions_in_area(900, 2500, 6699, 10350, blueminionpos),
+        #  "blue_side_top_l2_red_minions": minions_in_area(900, 2500, 6699, 10350, redminionpos),
+        #  "blue_side_top_l3_blue_minions": minions_in_area(900, 2500, 4252, 6699, blueminionpos),
+        #  "blue_side_top_l3_red_minions": minions_in_area(900, 2500, 4252, 6699, redminionpos),
+        #  "blue_side_mid_l2_blue_minions": minions_in_area(5048, 5800, 4812, 6200, blueminionpos),
+        #  "blue_side_mid_l2_red_minions": minions_in_area(5048, 5800, 4812, 6200, redminionpos),
+        #  "blue_side_mid_l3_blue_minions": minions_in_area(3620, 5048, 3696, 4812, blueminionpos),
+        #  "blue_side_mid_l3_red_minions": minions_in_area(3651, 5048, 3696, 4812, redminionpos),
+        #  "blue_side_bot_l2_blue_minions": minions_in_area(6919, 10400, 900, 2500, blueminionpos),
+        #  "blue_side_bot_l2_red_minions": minions_in_area(6919, 10400, 900, 2500, redminionpos),
+        #  "blue_side_bot_l3_blue_minions": minions_in_area(4281, 6919, 900, 2500, blueminionpos),
+        #  "blue_side_bot_l3_red_minions": minions_in_area(4281, 6919, 900, 2500, redminionpos),
+        #  "blue_side_base_blue_minions": minions_in_area(0, 3651, 0, 3696, blueminionpos),
+        #  "blue_side_base_red_minions": minions_in_area(0, 3651, 0, 3696, redminionpos),
+        #  "red_side_top_l2_blue_minions": minions_in_area(4550, 7952, 12500, 14000, blueminionpos),
+        #  "red_side_top_l2_red_minions": minions_in_area(4550, 7952, 12500, 14000, redminionpos),
+        #  "red_side_top_l3_blue_minions": minions_in_area(7952, 10481, 12500, 14000, blueminionpos),
+        #  "red_side_top_l3_red_minions": minions_in_area(7952, 10481, 12500, 14000, redminionpos),
+        #  "red_side_mid_l2_blue_minions": minions_in_area(8955, 9767, 8510, 10113, blueminionpos),
+        #  "red_side_mid_l2_red_minions": minions_in_area(8955, 9767, 8510, 10113, redminionpos),
+        #  "red_side_mid_l3_blue_minions": minions_in_area(9767, 11134, 10113, 11207, blueminionpos),
+        #  "red_side_mid_l3_red_minions": minions_in_area(9767, 11134, 10113, 11207, redminionpos),
+        #  "red_side_bot_l2_blue_minions": minions_in_area(12000, 14500, 4505, 8226, blueminionpos),
+        #  "red_side_bot_l2_red_minions": minions_in_area(12000, 14500, 4505, 8226, redminionpos),
+        #  "red_side_bot_l3_blue_minions": minions_in_area(12000, 14500, 8226, 11207, blueminionpos),
+        #  "red_side_bot_l3_red_minions": minions_in_area(12000, 14500, 8226, 11207, redminionpos),
+        #  "red_side_base_blue_minions": minions_in_area(11134, 11207, 18000, 18000, blueminionpos),
+        #  "red_side_base_red_minions": minions_in_area(11134, 11207, 18000, 18000, redminionpos),
+        #  "minion_advantage_top_inner": minionadv(red_top_inner, blue_top_inner),
+        #  "minion_advantage_mid_inner": minionadv(red_mid_inner, blue_mid_inner),
+        #  "minion_advantage_bot_inner": minionadv(red_bot_inner, blue_bot_inner),
+        #  "top_lane_pushed": top_wave_pushed,
+        #  "mid_lane_pushed": mid_wave_pushed,
+        #  "bot_lane_pushed": bot_wave_pushed
+    }
+    # print("Current Time:", gametime)
+    print("dump")
     for champ in champlist:
-        # s1 = str(champ.D.summoner_spell_type)
-        # s2 = str(champ.F.summoner_spell_type)
-        name = champ.name
-        # if name == 'monkeyking':
-        #     name = 'wukong'
-        team = champ.team
-        if team == 100:
-            team = "blue"
-        else:
-            team = "red"
         d['champs'].append({
-            "champion": name,
-            "x": champ.pos.x,
-            "y": champ.pos.y,
-            "z": champ.pos.z,
-            "items": [],
-            "team": team,
-            "current_hp": champ.health,
-            "max_hp": champ.max_health,
+            "champion": champ.name,
+            "x": round(champ.pos.x, 4),
+            "y": round(champ.pos.y, 4),
+            "z": round(champ.pos.z, 4),
+            # "items": [],
+            "team": team[champ.team],
+            "hp": round(champ.health, 4),
+            "max_hp": round(champ.max_health, 4),
             "is_alive": champ.is_alive,
             "is_visible": champ.is_visible,
             "is_recalling": champ.isRecalling,
-            "bonus_atk": champ.bonus_atk,
-            "armor": champ.armour,
-            "mr": champ.magic_resist,
+            "bonus_atk": round(champ.bonus_atk, 4),
+            "armor": round(champ.armour, 4),
+            "mr": round(champ.magic_resist, 4),
             "ap": champ.ap,
             "crit": champ.crit,
-            "crit_multi": champ.crit_multi,
+            # "crit_multi": champ.crit_multi,
             "atk_range": champ.atk_range,
-            "atk_speed_multi": champ.atk_speed_multi,
+            "atk_speed_multi": round(champ.atk_speed_multi, 4),
             "movement_speed": champ.movement_speed,
-            "last_visible_at": champ.last_visible_at,
-            #     "Q": {"level": champ.Q.level,
-            #         "ready_at": champ.Q.ready_at,
-            #         "speed": champ.Q.speed,
-            #         "cast_range": champ.Q.cast_range,
-            #         "width": champ.Q.width,
-            #         "delay": champ.Q.delay},
-            #     "W": {"level": champ.W.level,
-            #         "ready_at": champ.W.ready_at,
-            #         "speed": champ.W.speed,
-            #         "cast_range": champ.W.cast_range,
-            #         "width": champ.W.width,
-            #         "delay": champ.W.delay},
-            #     "E": {"level": champ.E.level,
-            #         "ready_at": champ.E.ready_at,
-            #         "speed": champ.E.speed,
-            #         "cast_range": champ.E.cast_range,
-            #         "width": champ.E.width,
-            #         "delay": champ.E.delay},
-            #     "R": {"level": champ.R.level,
-            #         "ready_at": champ.R.ready_at,
-            #         "speed": champ.R.speed,
-            #         "cast_range": champ.R.cast_range,
-            #         "width": champ.R.width,
-            #         "delay": champ.R.delay},
-            #     "D": {"name": s1,
-            #         "ready_at": champ.D.ready_at},
-            #     "F": {"name": s2,
-            #         "ready_at": champ.F.ready_at}
+            "Q": {
+                "level": champ.Q.level,
+                "ready_at": champ.Q.ready_at,
+                "width": champ.Q.width,
+                "delay": champ.Q.delay
+            },
+            "W": {
+                "level": champ.W.level,
+                "ready_at": champ.W.ready_at,
+                "width": champ.W.width,
+                "delay": champ.W.delay
+            },
+            "E": {
+                "level": champ.E.level,
+                "ready_at": champ.E.ready_at,
+                "width": champ.E.width,
+                "delay": champ.E.delay
+            },
+            "R": {
+                "level": champ.R.level,
+                "ready_at": champ.R.ready_at,
+                "width": champ.R.width,
+                "delay": champ.R.delay
+            },
+            "D": {
+                "name": champ.D.summoner_spell_type,
+                "ready_at": champ.D.ready_at
+            },
+            "F": {
+                "name": champ.F.summoner_spell_type,
+                "ready_at": champ.F.ready_at
+            }
         })
-
     # if obj.time == gametime and gametime > 20:
     #     results = list(db.temp.find())
     #     print("First")
@@ -297,7 +318,7 @@ def dump(obj):
     #     "index", -1), ("page_index", -1)], limit=1)['index'], "page_index": page_index, "snapshots": []})
     # db.temp.delete_many({})
     # else:
-    db.temp.insert_one(d)
+    # db.temp.insert_one(d)
     snapshot_index += 1
 
 
--- LViewPlus64/Offsets.cpp
+++ LViewPlus64/Offsets.cpp
@@ -6,61 +6,61 @@ for evade use spellbook activespell and/or missilelist
 
 Offsets::Offsets() {};
 
-int Offsets::GameTime						 = 0x517D598; // 15.7 F3 0F 5C 35 ?? ?? ?? ?? 0F 28 F8
+int Offsets::GameTime = 0x517D598; // 15.7 F3 0F 5C 35 ?? ?? ?? ?? 0F 28 F8
 
-int Offsets::ObjIndex                        = 0x10; //
-int Offsets::ObjTeam                         = 0x3C; //
-int Offsets::ObjMissileName                  = 0x54;
-int Offsets::ObjNetworkID                    = 0xC8; //
-int Offsets::ObjPos                          = 0x220; // 0xF8
-int Offsets::ObjMissileSpellCast             = 0x250;
-int Offsets::ObjVisibility                   = 0x310; // 0x274
-int Offsets::ObjSpawnCount                   = 0x328; // IsAlive = 0x21E6E0; isTargetable = 0xEB0; isVisible = 0x456f8ad0; ? ? ?
-int Offsets::ObjSrcIndex                     = 0x294;
-int Offsets::ObjMana                         = 0x0340; //
-int Offsets::ObjMaxMana						 = 0x0358; //
-int Offsets::ObjRecallState                  = 0xF40;
-int Offsets::ObjHealth                       = 0x1058; //
-int Offsets::ObjMaxHealth                    = 0x1070; //
-int Offsets::ObjAbilityHaste				 = 0x1480; //
-int Offsets::ObjLethality					 = 0x1580; //
-int Offsets::ObjArmor                        = 0x167C; //
-int Offsets::ObjBonusArmor					 = 0x1680; //
-int Offsets::ObjMagicRes                     = 0x1684; //
-int Offsets::ObjBonusMagicRes				 = 0x1688; //
-int Offsets::ObjBaseAtk                      = 0x1654; // 
-int Offsets::ObjBonusAtk                     = 0x15C0; //
-int Offsets::ObjMoveSpeed                    = 0x1694; //
-int Offsets::ObjSpellBook                    = 0x3118; // 0x3118
-int Offsets::ObjTransformation               = 0x3040;
-int Offsets::ObjName                         = 0x3898; //
-int Offsets::PlayerName						 = 0x60; //
-int Offsets::ObjLvl                          = 0x4038; // ????
-int Offsets::ObjExpiry                       = 0x298; 
-int Offsets::ObjCrit                         = 0x1858; //
-int Offsets::ObjCritMulti                    = 0x12D4; //
-int Offsets::ObjAbilityPower                 = 0x15D0; //
-int Offsets::ObjAtkSpeedMulti                = 0x1650; //
-int Offsets::ObjAtkRange                     = 0x1D50; //
-int Offsets::ObjTargetable					 = 0xEB0; //
-int Offsets::ObjInvulnerable				 = 0x4F0; //
-int Offsets::ObjIsMoving					 = 0x3638;
-int Offsets::ObjDirection					 = 0x3EB4; //
-int Offsets::ObjItemList					 = 0x35F8; //
+int Offsets::ObjIndex = 0x10; //
+int Offsets::ObjTeam = 0x3C; //
+int Offsets::ObjMissileName = 0x54;
+int Offsets::ObjNetworkID = 0xC8; //
+int Offsets::ObjPos = 0x220; // 0xF8
+int Offsets::ObjMissileSpellCast = 0x250;
+int Offsets::ObjVisibility = 0x310; // 0x274
+int Offsets::ObjSpawnCount = 0x328; // IsAlive = 0x21E6E0; isTargetable = 0xEB0; isVisible = 0x456f8ad0; ? ? ?
+int Offsets::ObjSrcIndex = 0x294;
+int Offsets::ObjMana = 0x0340; //
+int Offsets::ObjMaxMana = 0x0358; //
+int Offsets::ObjRecallState = 0xF40;
+int Offsets::ObjHealth = 0x1058; //
+int Offsets::ObjMaxHealth = 0x1070; //
+int Offsets::ObjAbilityHaste = 0x1480; //
+int Offsets::ObjLethality = 0x1580; //
+int Offsets::ObjArmor = 0x167C; //
+int Offsets::ObjBonusArmor = 0x1680; //
+int Offsets::ObjMagicRes = 0x1684; //
+int Offsets::ObjBonusMagicRes = 0x1688; //
+int Offsets::ObjBaseAtk = 0x1654; // 
+int Offsets::ObjBonusAtk = 0x15C0; //
+int Offsets::ObjMoveSpeed = 0x1694; //
+int Offsets::ObjSpellBook = 0x3118; // 0x3118
+int Offsets::ObjTransformation = 0x3040;
+int Offsets::ObjName = 0x3898; //
+int Offsets::PlayerName = 0x60; //
+int Offsets::ObjLvl = 0x4038; // ????
+int Offsets::ObjExpiry = 0x298;
+int Offsets::ObjCrit = 0x1858; //
+int Offsets::ObjCritMulti = 0x12D4; //
+int Offsets::ObjAbilityPower = 0x15D0; //
+int Offsets::ObjAtkSpeedMulti = 0x1650; //
+int Offsets::ObjAtkRange = 0x1D50; //
+int Offsets::ObjTargetable = 0xEB0; //
+int Offsets::ObjInvulnerable = 0x4F0; //
+int Offsets::ObjIsMoving = 0x3638;
+int Offsets::ObjDirection = 0x3EB4; //
+int Offsets::ObjItemList = 0x35F8; //
 
-int Offsets::ObjExpierience					 = 0x359C;
-int Offsets::ObjMagicPen					 = 0x1564;
-int Offsets::ObjMagicPenMulti				 = 0x1280;
-int Offsets::ObjAdditionalApMulti			 = 0x12E8; 
-int Offsets::ObjManaRegen					 = 0x11E8;
-int Offsets::ObjHealthRegen					 = 0x1690; //
+int Offsets::ObjExpierience = 0x359C;
+int Offsets::ObjMagicPen = 0x1564;
+int Offsets::ObjMagicPenMulti = 0x1280;
+int Offsets::ObjAdditionalApMulti = 0x12E8;
+int Offsets::ObjManaRegen = 0x11E8;
+int Offsets::ObjHealthRegen = 0x1690; //
 
-int Offsets::Ping							 = 0x51AD648; //
-int Offsets::OffPing						 = 0x178; //
-int Offsets::ShowPing						 = 0x6D8; //
+int Offsets::Ping = 0x51AD648; //
+int Offsets::OffPing = 0x178; //
+int Offsets::ShowPing = 0x6D8; //
 
-int Offsets::Chat							 = 0x518ECB8; //
-int Offsets::ChatIsOpen						 = 0xC90; //
+int Offsets::Chat = 0x518ECB8; //
+int Offsets::ChatIsOpen = 0xC90; //
 
 int Offsets::SpellBookActiveSpellCast = 0x2A00; // ManaCosts = 0x90;
 int Offsets::SpellBookSpellSlots = 0x4C8; // Spellbook spells
@@ -78,9 +78,9 @@ int Offsets::BuffName = 0x8;
 int Offsets::BuffEntryBuffNodeStart = 0x20;
 int Offsets::BuffEntryBuffNodeCurrent = 0x24;
 
-int Offsets::ItemListItem                 = 0xC;
-int Offsets::ItemInfo                     = 0x20;
-int Offsets::ItemInfoId                   = 0x68;
+int Offsets::ItemListItem = 0xC;
+int Offsets::ItemInfo = 0x20;
+int Offsets::ItemInfoId = 0x68;
 
 int Offsets::CurrentDashSpeed = 0x1D0;
 int Offsets::IsDashing = 0x398;
@@ -111,16 +111,16 @@ int Offsets::ObjectManager = 0x20D0E78; // 15.7 [actual address in first opcode]
 
 int Offsets::HeroList = 0x20D0FA0; //0x20D5978
 int Offsets::MinionList = 0x392B8A0; //
-int Offsets::MissileList = 0x5179250; //
-int Offsets::TurretList = 0x517d990; //
+int Offsets::MissileList = 0x517d990; //
+int Offsets::TurretList = 0x5179250; //
 
 int Offsets::LocalPlayer = 0x518AAD8; // 15.7 48 8B 3D ?? ?? ?? ?? 48 3B CF
 int Offsets::UnderMouseObject = 0x392BA88; // 15.7 48 89 0D ? ? ? ? 48 8D 05 ? ? ? ? 48 89 01 33 D2
 
-int Offsets::ObjectMapCount               = 0x48; //
-int Offsets::ObjectMapRoot                = 0x40; //
-int Offsets::ObjectMapNodeNetId           = 0x20; // NodeFlags = 0x18;
-int Offsets::ObjectMapNodeObject          = 0x28; //
+int Offsets::ObjectMapCount = 0x48; //
+int Offsets::ObjectMapRoot = 0x40; //
+int Offsets::ObjectMapNodeNetId = 0x20; // NodeFlags = 0x18;
+int Offsets::ObjectMapNodeObject = 0x28; //
 
 int Offsets::SpellCastSpellInfo = 0x8;
 int Offsets::SpellCastStartTime = 0x544;
@@ -137,10 +137,10 @@ int Offsets::MissileDestIdx = 0x310;
 int Offsets::MissileStartPos = 0x2D0;
 int Offsets::MissileEndPos = 0x2DC;
 
-int Offsets::MinimapObject                = 0x5181C88; //
-int Offsets::MinimapObjectHud             = 0x320; //
-int Offsets::MinimapHudPos                = 0x58; // WorldSize = 0x28;
-int Offsets::MinimapHudSize               = 0x68; // ScreenOffset = 0x60;
+int Offsets::MinimapObject = 0x5181C88; //
+int Offsets::MinimapObjectHud = 0x320; //
+int Offsets::MinimapHudPos = 0x58; // WorldSize = 0x28;
+int Offsets::MinimapHudSize = 0x68; // ScreenOffset = 0x60;
 
 int Offsets::AiManager = 0x2BDC;   // VT 151
 int Offsets::AiManagerStartPath = 0x1cc;
